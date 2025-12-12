e18e1098c5f4373f80eaf4071ffcad06419c2fcb
dharshini <maalusiva.092gmail.com>
2025-12-12 09:46:15 +0530

    second push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from"./components/pages/Header";
+import Footer from"./components/Footer";
+import Home from"./components/pages/Home";
+const App=()=>{
+  return(
+    <>
+    <Header/>
+    <Footer/>
+    <Home name= "charu"/>
+    
+    <h1>Hello</h1>
+    <h1>Hello</h1>
+    </>
+  )
+}
+export default App;
+
+
+    
+  
+
+
---
+++ frontend/src/components/pages/Hooks/Usestate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Usestate = () => {
+    const [count,setCount]=useState(0);
+  return (
+    <div>
+        UseState
+      
+    </div>
+  )
+}
+
+export default Usestate
